74bdafc016b0e4a0a63c8821fae0ceabd9e408f3
HiroyukiWindows <nevertoolatetostartsomething@gmail.com>
2022-04-24 17:04:58 +0900

    routing設定完了時点

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import ProductPage from "./components/pages/ProductPage";
+import HomePage from "./components/pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Routes>
+      <Route path="/products" element={<ProductPage />} exact />
+      <Route path="/" element={<HomePage />} exact />
+    </Routes>
   );
 }
 
---
+++ src/components/pages/ProductPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductPage() {
+  return <h1>商品ページ</h1>;
+}
+
+export default ProductPage;
