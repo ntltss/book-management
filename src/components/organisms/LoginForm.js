59411bd90aa52a645b03589f4ad3946a978722e3
HiroyukiWindows <nevertoolatetostartsomething@gmail.com>
2022-05-02 21:53:55 +0900

    login時にAPIでチェックするように実装完了時点

--- src/components/organisms/LoginForm.js
+++ src/components/organisms/LoginForm.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   makeStyles,
   Box,
@@ -7,7 +8,6 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
-import React from "react";
 
 import { Link } from "react-router-dom";
 
@@ -19,6 +19,28 @@ const useStyles = makeStyles((theme) => ({
 
 const LoginForm = () => {
   const classes = useStyles();
+  const [loginId, setLoginId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const checkInput = () => {
+    console.log("Check!");
+    console.log("loginId", loginId);
+    console.log("password", password);
+    fetch("http://localhost:8082/bookshelf/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        loginId: loginId,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (loginId === json.loginId && password === json.password) {
+          window.location.href = "/books";
+        }
+      });
+  };
   return (
     <>
       <Box mx={"auto"} my={4} className={classes.root}>
@@ -30,18 +52,31 @@ const LoginForm = () => {
             <Divider />
             <Box py={2}>
               <Box my={2}>
-                <TextField variant="outlined" label={"Eメールアドレス"} />
+                <TextField
+                  variant="outlined"
+                  label={"Eメールアドレス"}
+                  onChange={(e) => {
+                    setLoginId(e.target.value);
+                  }}
+                />
               </Box>
               <Box my={2}>
-                <TextField variant="outlined" label={"パスワード"} />
+                <TextField
+                  variant="outlined"
+                  label={"パスワード"}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
               </Box>
             </Box>
             <Box my={2}>
               <Button
                 color="primary"
                 variant="contained"
-                component={Link}
-                to="/books"
+                onClick={checkInput}
+                // component={Link}
+                // to="/books"
               >
                 ログイン
               </Button>
